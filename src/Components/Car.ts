aa91cf9e85fd789ad151886fac1d0358a4047657
John Turner <johnzilla@users.noreply.github.com>
2025-06-19 20:25:08 -0400

    Updated Car.ts

--- src/Components/Car.ts
+++ src/Components/Car.ts
@@ -14,6 +14,11 @@ export class Car {
 	public percent: number = 0;
 	public scale: number = 1500;
 
+	// New fields to support merging
+	public isMerging: boolean = false;
+	public hasMerged: boolean = false;
+	public targetOffset: number = 0;
+
 	constructor(scene: GameScene, road: Road, offset: number, trackPosition: number, sprite: string, speed: number) {
 		this.scene = scene;
 		this.road = road;
@@ -32,87 +37,28 @@ export class Car {
 		this.updateAngleFrame(carSegment, playerSegment, playerOffset);
 	}
 
-public draw(x: number = 0, y: number = 0, scale: number = 1, segmentClip: number = 0) {
-	const roadCenterX = this.scene.scale.width / 2;
-	const roadHalfWidth = gameSettings.roadWidth * scale / 2;
+	public draw(x: number = 0, y: number = 0, scale: number = 1, segmentClip: number = 0) {
+		const roadCenterX = this.scene.scale.width / 2;
+		const roadHalfWidth = gameSettings.roadWidth * scale / 2;
+		const screenX = roadCenterX + this.offset * roadHalfWidth;
 
-	const screenX = roadCenterX + this.offset * roadHalfWidth;
+		this.sprite.setPosition(screenX, y);
+		this.sprite.setScale(this.scale * scale);
+		this.sprite.setDepth(10 + scale);
 
-	this.sprite.setPosition(screenX, y);
-	this.sprite.setScale(this.scale * scale);
-	this.sprite.setDepth(10 + scale);
+		if (!this.sprite.visible) {
+			this.sprite.setVisible(true);
+		}
 
-	if (!this.sprite.visible) {
-		this.sprite.setVisible(true);
+		if (y > segmentClip) {
+			const clipped = (y - segmentClip) / this.sprite.scaleY;
+			const cropY = this.sprite.height - clipped;
+			this.sprite.setCrop(0, 0, this.sprite.width, cropY);
+		} else {
+			this.sprite.setCrop();
+		}
 	}
-
-	if (y > segmentClip) {
-		const clipped = (y - segmentClip) / this.sprite.scaleY;
-		const cropY = this.sprite.height - clipped;
-		this.sprite.setCrop(0, 0, this.sprite.width, cropY);
-	} else {
-		this.sprite.setCrop();
-	}
-}
 
 	public updateAngleFrame(carSegment: TrackSegment, playerSegment: TrackSegment, playerOffset: number): void {
 		const roadDistance = Math.abs(carSegment.index - playerSegment.index);
-		const offsetDistance = Math.abs(playerOffset - this.offset);
-		const isLeft = playerOffset > this.offset;
-
-		if (roadDistance < 20 && offsetDistance > 0.3) {
-			this.sprite.setFrame(1);
-			this.sprite.flipX = !isLeft;
-		} else {
-			this.sprite.setFrame(0);
-		}
-	}
-
-	public updateOffset(delta: number, carSegment: TrackSegment, playerSegment: TrackSegment): void {
-		// segments ahead to see if there's somethign to avoid
-		const lookahead = 50;
-
-		const player = this.scene.player;
-
-		// car not visible, don't do ai behaviour
-		if (carSegment.index - playerSegment.index > gameSettings.drawDistance) {
-			return;
-		}
-
-		for (let i = 0; i < lookahead; i++) {
-			const segment = this.road.segments[ (carSegment.index + i) % this.road.segments.length];
-
-      if (!this.hasMerged && segment === playerSegment && this.speed > player.speed && Util.overlapPlayer(player, this)) {
-	      if (player.x < this.offset) {
-		      this.offset = Util.interpolate(this.offset, 1, delta * 0.1);
-	      } else {
-		      this.offset = Util.interpolate(this.offset, -1, delta * 0.1);
-	      }
-      }
-
-			if (segment.cars.size) {
-				segment.cars.forEach((car: Car) => {
-					if (car === this) { return; }
-
-          if (!this.hasMerged && this.speed > car.speed && Util.overlapSprite(car.sprite, this.sprite)) {
-						if (car.offset < this.offset) {
-							this.offset = Util.interpolate(this.offset, 1, delta * 0.1);
-						} else {
-							this.offset = Util.interpolate(this.offset, -1, delta * 0.1);
-						}
-					}
-				});
-			}
-
-		}
-
-		// steer towards center of track if outside it
-		if (Math.abs(this.offset) > 0.9) {
-			this.offset = Util.interpolate(this.offset, 0, delta);
-		}
-	}
-
-	public destroy(): void {
-		this.sprite.destroy();
-	}
-}
+		const offsetDistance =
